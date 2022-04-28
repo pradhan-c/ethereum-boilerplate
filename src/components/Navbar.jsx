88052bb7a5a4a4121737da96004c98b72c09560c
Pradhan <chrisonpradhan@gmail.com>
2022-04-28 17:31:13 +0530

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navigation = ({ web3Handler, account }) => {
     return (
         <Navbar expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="http://www.dappuniversity.com/bootcamp">
+                <Navbar.Brand>
                     <img src={market} width="40" height="40" className="" alt="" className="d-inline-block align-top"/>{' '}ClosedSea
                    
                 </Navbar.Brand>
