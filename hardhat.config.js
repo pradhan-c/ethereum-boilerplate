fdbc724e7a79b8a89ad4eb8fb5e36bf5cea2c3c3
Pradhan <chrisonpradhan@gmail.com>
2022-05-04 10:36:03 +0530

    delete comment lines

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("@nomiclabs/hardhat-waffle");
 
 
 const Alchemy_API_Key = "https://eth-rinkeby.alchemyapi.io/v2/bztw3gURvaNVRGo5xsWKgBvIj0_67aAz"
-const Private_Key = "e7bc9bf6e1fe20f7888b54c496ac8f67da1ba0826d46a5e515b83c410a67c97b";
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
